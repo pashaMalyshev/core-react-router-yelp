180f7d57c705913988044dc4087e6f5a74069db6
Michael Klishevich <busation@gmail.com>
2019-01-30 08:56:43 +0300

    updated

---
+++ src/server/app.js
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import express from 'express';
+import path from 'path';
+import proxy from 'http-proxy-middleware';
+import handlebars from 'handlebars';
+import config from './config/default';
+import router from './router';
+
+const app = express();
+const { buildConfig: { assetsDir, targetDir }, server: { port }, proxyAssets } = config;
+
+if (config.appModeDev) {
+  app.use(
+    `/${assetsDir}`,
+    proxy({ target: `http://${proxyAssets.host}:${proxyAssets.port}`, changeOrigin: true }),
+  );
+} else {
+  app.use(
+    `/${assetsDir}`,
+    express.static(path.join(process.cwd(), targetDir, 'client')),
+  );
+}
+
+app.use('/api', router);
+
+app.use('*', (req, res) => {
+  const template = handlebars.compile(fs.readFileSync(
+    path.join(__dirname, 'index.hbs'),
+    'utf8',
+  ));
+  const context = {
+    title: 'Express React Skeleton'
+  };
+  res.send(template(context));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
